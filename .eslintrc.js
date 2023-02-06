1871cf5705b357b04e8e0d394fe0a5b1bd6a694d
chinyday <chinyday@gmail.com>
2023-02-06 23:09:47 +0900

    라우터 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,13 +19,14 @@ module.exports = {
     'react',
   ],
   rules: {
+    'import/no-extraneous-dependencies': 'off',
     'no-console': [0], // turned off for now while we are console.logging everywhere.
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-      },
-    ],
+    // 'import/no-extraneous-dependencies': [
+    //   'error',
+    //   {
+    //     devDependencies: true,
+    //   },
+    // ],
     // react eslint rules off
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
